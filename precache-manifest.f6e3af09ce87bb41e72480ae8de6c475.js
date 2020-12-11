7fbac3c809e77b2bdd0db999ff614ac682628afb
iamkryptonite <iamkryptonite@users.noreply.github.com>
2020-12-11 22:10:20 +0530

    Updates

---
+++ precache-manifest.f6e3af09ce87bb41e72480ae8de6c475.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1b3f368453a4c7d52176f50b37c7348",
+    "url": "/anitab-org.github.io/index.html"
+  },
+  {
+    "revision": "e9ade112c3225f77ca4d",
+    "url": "/anitab-org.github.io/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "ae03da5e761411bf9401",
+    "url": "/anitab-org.github.io/static/js/2.f5e89ec0.chunk.js"
+  },
+  {
+    "revision": "6d5752827b48785282474b643cdf2ee2",
+    "url": "/anitab-org.github.io/static/js/2.f5e89ec0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9ade112c3225f77ca4d",
+    "url": "/anitab-org.github.io/static/js/main.843f9de4.chunk.js"
+  },
+  {
+    "revision": "f849ebd811bad00d0ce7",
+    "url": "/anitab-org.github.io/static/js/runtime-main.f14f3321.js"
+  },
+  {
+    "revision": "4e44d31d9d13d72cdf03685e1f8ae53d",
+    "url": "/anitab-org.github.io/static/media/about.4e44d31d.png"
+  },
+  {
+    "revision": "552c358973068e62c7a41bbbcce2fc67",
+    "url": "/anitab-org.github.io/static/media/contribute.552c3589.png"
+  },
+  {
+    "revision": "5a653ec391ddcff0753c57d08a3c2eb9",
+    "url": "/anitab-org.github.io/static/media/linkedin-logo.5a653ec3.png"
+  },
+  {
+    "revision": "45cc4ce0406ddd458e03a61f586ea483",
+    "url": "/anitab-org.github.io/static/media/logo.45cc4ce0.png"
+  },
+  {
+    "revision": "b19b61ad9e12c23fe0e4f6c34b9d1f69",
+    "url": "/anitab-org.github.io/static/media/our-projects.b19b61ad.png"
+  },
+  {
+    "revision": "66c6a83d8e650fe44c591ef600271c13",
+    "url": "/anitab-org.github.io/static/media/placeholder.66c6a83d.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c3a29b1d80ad4da47479572cbb20b41b.js"
+  "/anitab-org.github.io/precache-manifest.f6e3af09ce87bb41e72480ae8de6c475.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/anitab-org.github.io/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
